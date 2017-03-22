2f2aad4d9a89fc54e1c3eb64cb0245fbd0e96c09
lilyRuan <xiqiruan@gmail.com>
2017-03-22 02:51:47 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -32,3 +32,8 @@ cacheSolve <- function(x, ...) {
   x$setinverse(i)
   i
 }
+## sample code to run: myMatrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+## cacheSolve(myMatrix)
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
